f466c09f87dd5e7de9a03b00fc3b61ebf3e6b120
허냄뚜 <132042040+Heonamkyu93@users.noreply.github.com>
2024-02-23 21:15:01 +0900

    [Bronze V] Title: 합, Time: 120 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+        int a=Integer.parseInt(reader.readLine());
+        
+        System.out.println((long)a*(a+1)/2);
+    }
+}
