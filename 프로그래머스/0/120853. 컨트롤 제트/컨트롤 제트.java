1c32e016556b4ef9831af3093f1d65f1ef9bc6c8
허냄뚜 <132042040+Heonamkyu93@users.noreply.github.com>
2024-03-15 08:29:53 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.12 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] a=s.split(" ");
+        for(int i=0;i<a.length;i++){
+            if(a[i].equals("Z")) {answer-=Integer.parseInt(a[i-1]); continue;}
+            answer+=Integer.parseInt(a[i]);
+        }
+        
+        
+        return answer;
+    }
+}
